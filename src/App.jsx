0a1816fab3f503bf112490482a3a765facb95845
Smit Parmar <smit.parmar999@gmail.com>
2024-06-25 18:54:24 +0530

    basic practice of props, state and useState hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,35 @@
 import { CORE_CONCEPTS } from './data';
 import Header from '../src/components/Header/Header.jsx';
 import CoreConcept from './components/CoreConcept.jsx'
+import TabButton from './components/TabButton.jsx';
+import { useState } from 'react';
+import { EXAMPLES } from './data';
+
 
 function App() {
+
+  const [selectedTopic, setSelectedTopic] = useState();
+
+  function handleClick(selectedButton) {
+    //selectedButton => 'components','JSX','props','state'
+    setSelectedTopic(selectedButton);
+  }
+
+  // Third Approach to render the content conditionally.
+  // let tabContent = <p>Please select the content.</p>;
+
+  // if(selectedTopic){
+  //   tabContent = (
+  //   <div id="tab-content">
+  //     <h3>{EXAMPLES[selectedTopic].title}</h3>
+  //       <p>{EXAMPLES[selectedTopic].description}</p>
+  //       <pre>
+  //         <code>{EXAMPLES[selectedTopic].code}</code>
+  //       </pre>
+  //     </div>
+  //     );
+  // }
+
   return (
     <div>
       <Header />
@@ -15,11 +42,48 @@ function App() {
               description={CORE_CONCEPTS[0].description}
               image={CORE_CONCEPTS[0].image}
             />
-            <CoreConcept {...CORE_CONCEPTS[1]}/> 
-            <CoreConcept {...CORE_CONCEPTS[2]}/>
-            <CoreConcept {...CORE_CONCEPTS[3]}/>
-
+            <CoreConcept {...CORE_CONCEPTS[1]} />
+            <CoreConcept {...CORE_CONCEPTS[2]} />
+            <CoreConcept {...CORE_CONCEPTS[3]} />
           </ul>
+        </section>
+
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            {/* <TabButton label='My Component' /> */}
+            <TabButton onSelect={() => handleClick('components')}>Components</TabButton>
+            <TabButton onSelect={() => handleClick('jsx')}>JSX</TabButton>
+            <TabButton onSelect={() => handleClick('props')}>Props</TabButton>
+            <TabButton onSelect={() => handleClick('state')}>State</TabButton>
+          </menu>
+
+          {/* First Approach to render content conditionally */}
+          {!selectedTopic && 'Please select the topic.'}
+          {selectedTopic && <div id="tab-content">
+          <h3>{EXAMPLES[selectedTopic].title}</h3>
+            <p>{EXAMPLES[selectedTopic].description}</p>
+            <pre>
+              <code>{EXAMPLES[selectedTopic].code}</code>
+            </pre>
+          </div>
+          }
+
+
+          {/* Second Approach to render content conditionally */}
+
+          {/* {!selectedTopic ? 'Please select the topic.' : null}
+          {selectedTopic ? <div id="tab-content">
+          <h3>{EXAMPLES[selectedTopic].title}</h3>
+            <p>{EXAMPLES[selectedTopic].description}</p>
+            <pre>
+              <code>{EXAMPLES[selectedTopic].code}</code>
+            </pre>
+          </div> : null} */}
+
+          {/* Third Approach */}
+          {/* {tabContent} */}
+          
         </section>
         <h2>Time to get started!</h2>
       </main>
